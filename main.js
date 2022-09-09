44675ba83a229bbb0fcd19a0c52068782f6479ca
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-09-09 18:21:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3Wka68qox/model.json", modelLoaded);
 function modelLoaded() {
     console.log("Model loaded");
+}Webcam.attach('#camera');
+camera= document.getElementById("camera");
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+function takeSnapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("foto").innerHTML= '<img id="selfie_image" src= "'+data_uri+'"/>';
+    });
+}
+function modelLoaded() {
+    console.log("Model loaded");
 }
