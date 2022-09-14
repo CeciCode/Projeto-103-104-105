24894cc1e340c2bfbe0163c969a822faf574efd6
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-09-14 15:01:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ function takeSnapshot() {
 function modelLoaded() {
     console.log("Model loaded");
 }
+function check() {
+    img= document.getElementById("selfie_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("identificado").innerHTML= results[0].label;
+        percent= results[0].confidence.toFixed(3) * 100;
+        console.log(percent);
+        document.getElementById("precisao").innerHTML= percent + "%";
+        object= results[0].label;
+    }
+}
